9f16436b98f48288c931cbf189005bd7b32a1eb4
Kyohei Nakafuji <kyohei.nakafuji@ssie.jp>
2017-06-27 16:49:52 +0900

    service worker実装

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('js/').then(function(registration) {
+  navigator.serviceWorker.register('sw.js').then(function(registration) {
     console.log('スコープをServiceWorkerに登録成功: ', registration.scope);
   }).catch(function(err) {
     console.log('ServiceWorkerに登録失敗: ', err);
---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '',
+  'js/sample.js'
+];
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
